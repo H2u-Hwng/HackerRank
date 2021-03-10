66abfb8054a2db2f58786d1a5229700e508db993
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-03-09 16:50:45 -0800

    Create 008. Set .difference() Operation.py

---
+++ Python/04. Sets/008. Set .difference() Operation.py
@@ -0,0 +1,9 @@
+# Problem: https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+# Score: 10.0
+
+
+n = int(input())
+set_n = set(input().split())
+b = int(input())
+set_b = set(input().split())
+print(len(set_n - set_b))
