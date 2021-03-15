be37f063ead1305b1cb86a318ce741075fabf666
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-03-14 23:45:54 -0700

    Update 005. Lists.py

--- Python/02. Basic Data Types/005. Lists.py
+++ Python/02. Basic Data Types/005. Lists.py
@@ -2,3 +2,15 @@
 # Score: 10.0
 
 
+n = int(input())
+arr = list()
+for _ in range(n):
+    cmd = input().split()
+    if cmd[0] == 'print':
+        print(arr)
+    elif len(cmd) == 1:
+        getattr(arr, cmd[0])()
+    elif len(cmd) == 2:
+        getattr(arr, cmd[0])(int(cmd[1]))
+    elif len(cmd) == 3:
+        getattr(arr, cmd[0])(int(cmd[1]), int(cmd[2]))
