de1585a8b0683c44471e314e7613958503edb9aa
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-03-19 23:11:36 -0700

    Create 001. Calendar Module.py

---
+++ Python/08. Date and Time/001. Calendar Module.py
@@ -0,0 +1,7 @@
+# Problem: https://www.hackerrank.com/challenges/calendar-module/problem
+# Score: 10.0
+
+
+import calendar
+m,d,y = map(int, input().split())
+print(calendar.day_name[calendar.weekday(y,m,d)].upper())
