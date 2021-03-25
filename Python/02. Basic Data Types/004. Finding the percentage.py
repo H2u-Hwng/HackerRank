42eb1aa2f861c5747b741ef4cbba4dfe72da674b
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-03-24 22:29:06 -0700

    Update 004. Finding the percentage.py

--- Python/02. Basic Data Types/004. Finding the percentage.py
+++ Python/02. Basic Data Types/004. Finding the percentage.py
@@ -2,3 +2,14 @@
 # Score: 10.0
 
 
+from statistics import mean
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+query_scores = student_marks[query_name]
+result = mean(query_scores)
+print('{0:.2f}'.format(result))
