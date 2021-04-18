4c180f347fd7ffbcf372ef8fecf822306c6e3804
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-04-18 13:46:54 -0700

    Create 005. Power - Mod Power.py

---
+++ Python/05. Math/005. Power - Mod Power.py
@@ -0,0 +1,7 @@
+# Problem: https://www.hackerrank.com/challenges/python-power-mod-power/problem
+# Score: 10.0
+
+
+a, b = int(input()), int(input())
+m = int(input())
+print(pow(a,b), pow(a,b,m), sep='\n')
