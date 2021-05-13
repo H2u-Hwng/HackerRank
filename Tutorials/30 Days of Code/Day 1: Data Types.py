7ffd228728fe93fb16b2c2327cba923aa9515835
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-05-13 10:09:43 -0700

    Create Day 1: Data Types.py

---
+++ Tutorials/30 Days of Code/Day 1: Data Types.py
@@ -0,0 +1,13 @@
+# Problem: https://www.hackerrank.com/challenges/30-data-types/problem
+# Score: 30.0
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+x = int(input())
+y = float(input())
+z = input()
+
+print(i+x, d+y, s+z, sep='\n') 
