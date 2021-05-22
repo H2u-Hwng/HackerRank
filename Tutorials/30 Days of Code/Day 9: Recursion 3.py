67503c1bd1e98a9794d4a906a18dbb1d4c1c0ce9
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-05-21 23:10:44 -0700

    Create Day 9: Recursion 3.py

---
+++ Tutorials/30 Days of Code/Day 9: Recursion 3.py
@@ -0,0 +1,12 @@
+# Problem: https://www.hackerrank.com/challenges/30-recursion/problem
+# Score: 30.0
+
+
+def factorial(n):
+    result = 1
+    for i in range(1,n+1):
+        result = result*i
+    print(result)
+
+n = int(input().strip())
+result = factorial(n)
