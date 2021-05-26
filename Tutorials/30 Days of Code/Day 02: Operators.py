b58d9c5799db8447501ce604f34a501be30c0eaa
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-05-25 22:41:18 -0700

    Update and rename Day 2: Operators.py to Day 02: Operators.py

--- Tutorials/30 Days of Code/Day 02: Operators.py
+++ Tutorials/30 Days of Code/Day 02: Operators.py
@@ -2,20 +2,11 @@
 # Score: 30.0
 
 
-import math
-import os
-import random
-import re
-import sys
-
-
 def solve(meal_cost, tip_percent, tax_percent):
     tip = tip_percent * meal_cost / 100
     tax = tax_percent * meal_cost / 100
     total_cost = meal_cost + tip + tax
-    
     print(round(total_cost))
-
 
 meal_cost = float(input())
 tip_percent = int(input())
