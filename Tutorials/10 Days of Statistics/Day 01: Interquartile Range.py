018f6730720043679d92916d1faad198b3a50315
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-05-30 23:17:58 -0700

    Update Day 01: Interquartile Range.py

--- Tutorials/10 Days of Statistics/Day 01: Interquartile Range.py
+++ Tutorials/10 Days of Statistics/Day 01: Interquartile Range.py
@@ -2,17 +2,16 @@
 # Score: 30.0
 
 
-import itertools
 from statistics import median
 
 n = int(input().strip())
 val = list(map(int, input().rstrip().split()))
 freq = list(map(int, input().rstrip().split()))
+
 res = []
 for i in range(n):
-    res2 = list(itertools.repeat(val[i],freq[i]))
-    for j in res2:
-        res.append(j)
+    res += [val[i]]*freq[i]
+
 res.sort()
 m = len(res)//2
 l = res[:m]
@@ -20,4 +19,5 @@ if n%2 == 0:
     u = res[m:]
 else:  
     u = res[m+1:]
+    
 print(round(float(median(u))-float(median(l)),1))
