5169ee8c820ca9e587dd58f063c490bec25ffa86
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-06-02 23:25:49 -0700

    Update 004. A Very Big Sum.py

--- Algorithms/01. Warmup/004. A Very Big Sum.py
+++ Algorithms/01. Warmup/004. A Very Big Sum.py
@@ -2,24 +2,10 @@
 # Score: 10.0
 
 
-import math
-import os
-import random
-import re
-import sys
-
 def aVeryBigSum(ar):
-    return sum(ar)
+    print(sum(ar))
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+ar_count = int(input().strip())
+ar = list(map(int, input().rstrip().split()))
 
-    ar_count = int(input().strip())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = aVeryBigSum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+result = aVeryBigSum(ar)
