2ddc69717ecb50ee8e5d00b4fd23c4cc5a3e7cc2
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-13 10:02:17 -0800

    Create 03. JavaSubstringComparisons.java

---
+++ Java/2. Strings/03. JavaSubstringComparisons.java
@@ -0,0 +1,25 @@
+# Problem: https://www.hackerrank.com/challenges/java-string-compare/problem
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static String getSmallestAndLargest(String s, int k) {
+        SortedSet<String> sets = new TreeSet<String>();
+        
+        for (int i = 0; i <= s.length() - k; i++) {
+            sets.add(s.substring(i, i + k));
+        }
+ 
+        return sets.first() + "\n" + sets.last();
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int n = in.nextInt();
+        
+        System.out.println(getSmallestAndLargest(s, n));
+    }
+}
