eef6e3db099dd5bc2c82f400e382fab2ab2431da
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-18 14:37:01 -0800

    Create 009. Designer_Door_Mat.py

---
+++ Python/03. Strings/009. Designer_Door_Mat.py
@@ -0,0 +1,7 @@
+# Problem: https://www.hackerrank.com/challenges/designer-door-mat/problem
+
+height, width = map(int, input().split())
+
+pattern = [('.|.' * (2 * i + 1)).center(width, '-') for i in range(height // 2)]
+
+print('\n'.join(pattern + ['WELCOME'.center(width, '-')] + pattern[::-1]))
